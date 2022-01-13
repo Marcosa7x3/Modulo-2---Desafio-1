86e606616968d6a5f8765af23a42c81d8d6f0982
Marcos Adriano <marcosadriano645@gmail.com>
2022-01-12 21:24:38 -0300

    Primeiro Desafio do Módulo 2 Vai na Web

---
+++ script.js
@@ -0,0 +1,7 @@
+let meuNome = "Marcos Adriano"
+let number = 10
+let anoAtual = 2022
+console.log(anoAtual - number)
+console.log("Estou mexendo no console.log")
+let quartaFeira = true
+console.log(quartaFeira)
